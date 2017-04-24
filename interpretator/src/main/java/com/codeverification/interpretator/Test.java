ff1065493134569984fccdd6f0dfcb1202f70cf4
Nazukin_Dmitry <nazukin.d@gmail.com>
2017-04-25 00:23:00 +0300

    interpreter continue3

--- interpretator/src/main/java/com/codeverification/interpretator/Test.java
+++ interpretator/src/main/java/com/codeverification/interpretator/Test.java
@@ -11,17 +11,16 @@ public class Test {
 //        System.out.println(Integer.decode("0b01010"));
         Caller caller = (Caller) LibraryProxy.newInstance(args[0], Caller.class);
         System.out.println(caller.Evkl(55,44));
-        System.out.println(caller.and(true,true));
+        System.out.println(caller.and(true,false));
         System.out.println(caller.calc(55,44, '+'));
         System.out.println(caller.concat("55","44"));
         System.out.println(caller.dummy());
-        System.out.println(caller.Factorial(10));
-        System.out.println(caller.Fib(10));
-        System.out.println(caller.FibByte((byte)10));
+        System.out.println(caller.Factorial(5));
+        System.out.println(caller.Fib(4));
+        System.out.println(caller.FibByte((byte)4));
         System.out.println(caller.fun1(10, 100));
         System.out.println(caller.hello("World"));
         System.out.println(caller.sto());
         System.out.println(caller.sum(-50,50));
-
     }
 }
